5564f02f94677795fcb72181c03d2d82f1317cd1
Mfaniseni-Thusi <kwazithusi6@gmail.com>
2019-07-29 18:05:11 +0200

    Exercise 3.2

--- complex-numbers.cpp
+++ complex-numbers.cpp
@@ -1,20 +1,24 @@
-#include <iostream> // contains the definition of cout, endl
-#include <complex> // contains the complex class definition
+// complex.cpp
+// Multiplying complex numbers
+#include <iostream>
+#include <complex> // required for the complex class
 
-using namespace std; // cout, endl, complex are all part of this namespace
-
+using namespace std;
 int main(){
-    auto num1 = complex<float>{2.0, 2.0}; // use auto for type deduction
-    auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax
-    auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
+
+    using complexType = complex<float>; // Used aliasing HERE
+    complexType num1{ 2.0, 2.0 }; // using C++11 uniform initialisation syntax
+    complexType num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+    auto answer = num1 * num2; // using C++11 auto keyword,
+    // answer is of type: complex<float>
 
     cout << "The answer is: " << answer << endl;
-    cout << "Or in a more familiar form: " << answer.real()
-    << " + " << answer.imag() << "j"
-    << endl << endl;
+    cout << "Or in a more familiar form: " << answer.real() << " + " <<
+    answer.imag() << "j" << endl;
     // answer++;
     return 0;
 }
+
 
 /* Answer to Exercise 3.1
   - The compiler is programmed to ignore every line that comes after // notation in a single line
