be754e7ba04cccedc52e45c85f4d0ca0f806269e
Mfaniseni-Thusi <kwazithusi6@gmail.com>
2019-07-29 19:07:30 +0200

    Exercise 4.6

--- exerc4-3main.cpp
+++ exerc4-3main.cpp
@@ -23,3 +23,15 @@ int main()
 
 	return 0;
 }
+
+/* 1. Change the string::string-type and use string
+   2. Instead of using auto we can explicitly declare the type of a variable - this helps improve the readability of the code.
+
+    NOT CHANGING PUBLIC INTERFACE:
+    - It helps keep improve the efficiency of the code without having the user to adapt to a new outlook
+      of a program every time there is a change to be implemented.
+
+    CHANGING IMPLEMENTATION:
+    - It gives a developer the ability to maintain their program and have control over it.
+      We are free to change it because it does not necessarily interfere with the user if approached rightly.
+*/
